8488839a7ecfb561d7803254c5a44e21a41e2fba
Wenfei Cao <cwfroom123@gmail.com>
2018-06-19 16:37:11 -0700

    reading dic file read and write

--- azPlayer/azPlayer/Model/MusicData.swift
+++ azPlayer/azPlayer/Model/MusicData.swift
@@ -10,13 +10,15 @@ import Foundation
 import MediaPlayer
 
 class SongInfo {
-    var songTitle:  String
-    var artistName: String
-    var albumTitle: String
+    var songTitle:  String;
+    var titleReading : String;
+    var artistName: String;
+    var albumTitle: String;
     var index : Int
     
-    init(songTitle : String, artistName : String, albumTitle : String, index : Int) {
+    init(songTitle : String, titleReading: String, artistName : String, albumTitle : String, index : Int) {
         self.songTitle = songTitle;
+        self.titleReading = titleReading;
         self.artistName = artistName;
         self.albumTitle = albumTitle;
         self.index = index;
@@ -32,12 +34,70 @@ class MusicData{
     var currentIndex : Int = 0;
     var sectionTitles : [String];
     var songDic : [String:[SongInfo]];
+    var readingDic : [String:String];
+    
+    let readingDicFileName = "ReadingDictionary.txt";
     
     init(){
         songs = [];
         sectionTitles = [];
         songDic = [:];
+        readingDic = [:];
+        loadReadingDic();
     };
+    
+    func getFileURL(fileName : String) -> URL{
+        let fileManager = FileManager.default;
+        if (!fileManager.fileExists(atPath: fileName)){
+            fileManager.createFile(atPath: fileName, contents: nil, attributes: nil);
+        }
+        let dir = fileManager.urls(for:.documentDirectory,in:.userDomainMask);
+        return (dir.first?.appendingPathExtension(fileName))!;
+    }
+    
+    private func loadReadingDic(){
+        readingDic = [:];
+        let fileURL = getFileURL(fileName: readingDicFileName);
+        var stringfied : String = "";
+        do{
+            stringfied = try String(contentsOf: fileURL,encoding:.utf8);
+        }catch{
+            print("Failed to load from reading dic");
+        }
+        let entries = stringfied.components(separatedBy: "\n");
+        for entry in entries {
+            print(entry+"\n");
+            let entrySplit = entry.components(separatedBy: " ");
+            if (entrySplit.count > 1){
+                readingDic[entrySplit[0]] = entrySplit[1];
+            }
+        }
+        
+    }
+    
+    private func saveReadingDic(){
+        let fileURL = getFileURL(fileName: readingDicFileName);
+        var stringfied : String = "";
+        for (title,reading) in readingDic {
+            stringfied += title + " " + reading + "\n";
+        }
+        do{
+            try stringfied.write(to: fileURL, atomically: false, encoding: .utf8);
+        }catch{
+            print("Failed to write to reading dic");
+        }
+    }
+    
+    private func deleteReadingDic(){
+        let fileManager = FileManager.default;
+        let dir = fileManager.urls(for:.documentDirectory,in:.userDomainMask);
+        let fileURL = (dir.first?.appendingPathExtension(readingDicFileName))!;
+        do{
+            try fileManager.removeItem(at: fileURL);
+        }catch{
+            print("Failed to delete reading dic");
+        }
+    }
     
     public func LoadSongs(songsTableView : SongsTableViewController){
         MPMediaLibrary.requestAuthorization { (status) in
@@ -46,7 +106,7 @@ class MusicData{
                 
                 for i in 0...songQuery.items!.count - 1{
                     let item = songQuery.items![i];
-                    
+                    let songTitle = item.title!;
                     var artist : String = "";
                     if (item.artist != nil){
                         artist = item.artist!
@@ -55,8 +115,14 @@ class MusicData{
                     if (item.albumTitle != nil){
                         albumTitle = item.albumTitle!
                     }
-                    let aSong : SongInfo = SongInfo(songTitle: item.title!, artistName: artist, albumTitle: albumTitle,index : i);
+                    var titleReading = songTitle;
+                    if let reading = self.readingDic[songTitle]{
+                        titleReading = reading;
+                    }
+                    
+                    let aSong : SongInfo = SongInfo(songTitle: songTitle, titleReading: titleReading, artistName: artist, albumTitle: albumTitle,index : i);
                     self.songs.append(aSong);
+                    
                     let sectionTitle = String(item.title!.prefix(1));
                     if var sectionSongs = self.songDic[sectionTitle]{
                         sectionSongs.append(aSong);
